a992a46846c974f20e2b91c34e26978534160e70
Ilya Shigabeev <imshigabeev@sberbank.ru>
2020-10-05 15:18:59 +0100

    error handling

--- src/backend.py
+++ src/backend.py
@@ -17,18 +17,22 @@ def handle_tts():
     input: {"text":"English text without normalization"}
     :return: wav file bytes
     """
-    content = request.get_json()
-    text = content.get('text')
-    write_result(text) # save request data
-    if type(text) != str:
-        return "Check the text.", 400
-    if len(text) > 1000:
-        return "Input length is too long.", 400
-    sr, wav = pronounce()
-    wav = np.multiply(wav, (2 ** 15)).astype(np.int16)
-    pcm = io.BytesIO()
-    wavfile.write(pcm, rate=sr, data=wav)
-    return send_file(pcm,
-                     as_attachment=True,
-                     attachment_filename='output.wav',
-                     mimetype='audio/wav')
+    try:
+        content = request.get_json()
+        text = content.get('text')
+        write_result(text) # save request data
+        if type(text) != str:
+            return "Check the text.", 400
+        if len(text) > 1000:
+            return "Input length is too long.", 400
+        sr, wav = pronounce()
+        wav = np.multiply(wav, (2 ** 15)).astype(np.int16)
+        pcm = io.BytesIO()
+        wavfile.write(pcm, rate=sr, data=wav)
+        return send_file(pcm,
+                         as_attachment=True,
+                         attachment_filename='output.wav',
+                         mimetype='audio/wav')
+    except Exception as e:
+        print(e.message, e.args)
+        return "Unexpected error occured. Message us to get a stable version of a product.", 500
