9888bb9f6f41a90bc905b1eaade28a193b19b3df
Ilya Shigabeev <imshigabeev@sberbank.ru>
2020-10-23 04:05:39 +0100

    cache voices

--- src/backend.py
+++ src/backend.py
@@ -1,4 +1,5 @@
 import io
+import os
 import json
 import logging
 
@@ -13,26 +14,42 @@ from utils import write_result
 
 app = Flask(__name__)
 CORS(app)
+CACHEDIR = os.path.dirname(os.path.abspath(__file__)) + "/cache"
+os.makedirs(CACHEDIR, exist_ok=True)
+
 
 @app.route('/tts', methods=['GET', 'POST'])
 def handle_tts():
     """
-    input: {"text":"English text without normalization"}
+    input: {"text":"English text without normalization",
+            "type": "wav|ogg" # TODO
+            "batched": 0 # TODO
+            "cached": 0 # TODO
+            }
     :return: wav file bytes
     """
     try:
         content = request.get_json()
         text = content.get('text')
+        audio_type = content.get('type', 'wav')
+
+        text_proc = text.strip()
+        fname = f"{hash(text_proc)}.{audio_type}"
+        fpath = f"{CACHEDIR}/{fname}"
+
         write_result(text) # save request data
         if type(text) != str:
-            return "Check the text.", 400
+            return "Input is incorrect. Check if it's the text.", 400
         if len(text) > 1000:
-            return "Input length is too long.", 400
-        sr, wav = pronounce(text)
-        wav = np.multiply(wav, (2 ** 15)).astype(np.int16)
-        pcm = io.BytesIO()
-        wavfile.write(pcm, rate=sr, data=wav)
-        return send_file(pcm,
+            return "Input length is too long. Message us to get a full version of a product.", 400
+
+        # Process sound
+        if not fname in os.listdir(CACHEDIR):
+            sr, wav = pronounce(text)
+            wav = np.multiply(wav, (2 ** 15)).astype(np.int16)
+            wavfile.write(fpath, rate=sr, data=wav)
+
+        return send_file(fpath,
                  as_attachment=True,
                  attachment_filename='output.wav',
                  mimetype='audio/wav')
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+import os
+
 def write_result(text):
-    with open("../data/history.txt", "a") as fs:
+    data_folder = "../data"
+    os.makedirs(data_folder, exist_ok=True)
+    with open(f"{data_folder}/history.txt", "a") as fs:
         fs.write(f'{text}\n')
